c36331607a429907cf5e95f3bce280bf12a17aa8
Maha24twq <mahasalem09.09.0@gmail.com>
2024-09-26 17:10:05 +0300

    Initial Commit

---
+++ hup/hupApp.swift
@@ -0,0 +1,17 @@
+//
+//  hupApp.swift
+//  hup
+//
+//  Created by Maha Salem Alghmadi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct hupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
